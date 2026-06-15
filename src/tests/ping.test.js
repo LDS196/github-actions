335f1847d44c5e2bded757f00bd5cf8656bfa9f0
lihachevd87@gmail.com <lihachevd87@gmail.com>
2026-06-15 07:55:48 +0300

    test: add whitespace for consistency in ping test case

--- src/tests/ping.test.js
+++ src/tests/ping.test.js
@@ -5,6 +5,7 @@ import app from '../index.js';
 describe('GET /ping', () => {
   it('should return pong', async () => {
     const res = await request(app).get('/ping');
+    
     expect(res.statusCode).toBe(200);
     expect(res.body.message).toBe('pong');
   });
